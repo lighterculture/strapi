0edcb4096e1fd41964d9f37abc58ca96cf19ef06
Pierre Noël <petersg83@gmail.com>
2022-04-29 09:51:55 +0200

    add update folder route

--- packages/core/upload/server/controllers/validation/admin/folder.js
+++ packages/core/upload/server/controllers/validation/admin/folder.js
@@ -7,6 +7,22 @@ const { getService } = require('../../../utils');
 const NO_SLASH_REGEX = /^[^/]+$/;
 const NO_SPACES_AROUND = /^(?! ).+(?<! )$/;
 
+const folderExists = async folderId => {
+  if (isNil(folderId)) {
+    return true;
+  }
+
+  const exists = await getService('folder').exists({ id: folderId });
+
+  return exists;
+};
+
+const isFolderUnique = async folder => {
+  const { exists } = getService('folder');
+  const doesExist = await exists({ parent: folder.parent || null, name: folder.name });
+  return !doesExist;
+};
+
 const validateCreateFolderSchema = yup
   .object()
   .shape({
@@ -19,24 +35,30 @@ const validateCreateFolderSchema = yup
     parent: yup
       .strapiID()
       .nullable()
-      .test('folder-exists', 'parent folder does not exist', async folderId => {
-        if (isNil(folderId)) {
-          return true;
-        }
-
-        const exists = await getService('folder').exists({ id: folderId });
-
-        return exists;
-      }),
+      .test('folder-exists', 'parent folder does not exist', folderExists),
   })
   .noUnknown()
   .required()
-  .test('is-folder-unique', 'name already taken', async folder => {
-    const { exists } = getService('folder');
-    const doesExist = await exists({ parent: folder.parent || null, name: folder.name });
-    return !doesExist;
-  });
+  .test('is-folder-unique', 'name already taken', isFolderUnique);
+
+const validateUpdateFolderSchema = yup
+  .object()
+  .shape({
+    name: yup
+      .string()
+      .min(1)
+      .matches(NO_SLASH_REGEX, 'name cannot contain slashes')
+      .matches(NO_SPACES_AROUND, 'name cannot start or end with a whitespace'),
+    parent: yup
+      .strapiID()
+      .nullable()
+      .test('folder-exists', 'parent folder does not exist', folderExists),
+  })
+  .noUnknown()
+  .required()
+  .test('is-folder-unique', 'name already taken', isFolderUnique);
 
 module.exports = {
   validateCreateFolder: validateYupSchema(validateCreateFolderSchema),
+  validateUpdateFolder: validateYupSchema(validateUpdateFolderSchema),
 };
